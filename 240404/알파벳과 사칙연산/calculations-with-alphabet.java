fff54863084b80b7104580367be7a5fae73cb144
김강년 <64246800+ekek54@users.noreply.github.com>
2024-04-04 14:23:06 +0900

    Title: 알파벳과 사칙연산, Time: 132ms, Memory: 9MB, Status: Passed - CodeTree

--- 240404/알파벳과 사칙연산/calculations-with-alphabet.java
+++ 240404/알파벳과 사칙연산/calculations-with-alphabet.java
@@ -3,29 +3,53 @@ import java.util.*;
 
 public class Main {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static int answer = Integer.MIN_VALUE;
+    private static int NUMS_SIZE = 6;
+    private static Stack<Integer> nums = new Stack<>();
+    private static String formular = "";
 
     public static void main(String[] args) throws IOException{
-        String formular = br.readLine();
-        int cur = 4;
-        for (int i = 1; i < formular.length(); i ++) {
-            if (formular.charAt(i) == '+') {
-                cur += 4;
-                continue;
-            }
-            if (formular.charAt(i) == '-') {
-                cur -= 1;
-                continue;
-            }
-            if (formular.charAt(i) == '*') {
-                if (cur >= 0) {
-                    cur *= 4;
-                    continue;
-                } else {
-                    cur *= 1;
-                    continue;
-                }
-            } else continue;
+        formular = br.readLine();
+        dfs(0);
+        System.out.println(answer);
+    }
+
+    private static void dfs(int cnt) {
+        if (cnt == NUMS_SIZE) {
+            answer = Math.max(answer, calcForm());
+            return;
         }
-        System.out.println(cur);
+
+        for (int i = 1; i <= 4; i++) {
+            nums.add(i);
+            dfs(cnt + 1);
+            nums.pop();
+        }
+    }
+
+    private static int calcForm() {
+        int cur = nums.get(idxByAlpha(formular.charAt(0)));
+        for (int i = 1; i < formular.length() - 1; i += 2) {
+            char operator = formular.charAt(i);
+            char operand = formular.charAt(i + 1);
+            int operandNum = nums.get(idxByAlpha(operand));
+            if (operator == '+') {
+                cur += operandNum;
+                continue;
+            }
+            if (operator == '-') {
+                cur -= operandNum;
+                continue;
+            }
+            if (operator == '*') {
+                cur *= operandNum;
+                continue;
+            }
+        }
+        return cur;
+    }
+
+    private static int idxByAlpha(char alpha) {
+        return (int)(alpha - 'a');
     }
 }
